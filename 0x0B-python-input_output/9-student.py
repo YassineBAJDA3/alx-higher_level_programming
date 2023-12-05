2e3ff04fa7a642cca1e30f22f799dab6d04ae08c
Yassine BAJDA <bajdamr@gmail.com>
2023-12-05 08:50:08 -0800

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,7 @@ class student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+    
     def to_json(self):
         """Return a dictionary representation of a student instance"""
         return self.__dict__
