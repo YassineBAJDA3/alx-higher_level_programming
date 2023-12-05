2a93fb412a909cf4a3a2aeb84d00ddd41bc2eda8
Yassine BAJDA <bajdamr@gmail.com>
2023-12-05 08:39:54 -0800

    9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""contains the class "student"""
+
+
+class student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student"""
+        self.first_name = fist_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return a dictionary representation of a student instance"""
+        return self.__dict__
