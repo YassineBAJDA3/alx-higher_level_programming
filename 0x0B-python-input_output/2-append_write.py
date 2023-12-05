74bc0fbc5553384b7c2beb99488613efea731db0
Yassine BAJDA <bajdamr@gmail.com>
2023-12-05 06:50:17 -0800

    2. Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """defining append_file with two arguments"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """append to file_name with UTF8"""
     with open(filename, "a", encoding='UTF8') as file:
         return file.write(text)
