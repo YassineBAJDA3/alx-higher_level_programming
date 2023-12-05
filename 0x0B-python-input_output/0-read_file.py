7f16e890702cc435373292798a722d4a5008a81f
Yassine BAJDA <bajdamr@gmail.com>
2023-12-05 06:32:04 -0800

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""defining read_file functin"""
 
 
 def read_file(filename=""):
-
+    """reads file_name with UTF8"""
     with open(filename, encoding = 'UTF8') as file:
         print(file.read(), end="")
