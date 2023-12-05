2484bbe0d932554838238350e627990973594ed1
Yassine BAJDA <bajdamr@gmail.com>
2023-12-05 08:20:16 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """task 7 MODUL"""
 
-
 import sys
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+# Importing functions without file extension
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arg_list = list(sys.argv[1:])
 
@@ -12,6 +13,6 @@ try:
     old_data = load_from_json_file('add_item.json')
 except Exception:
     old_data = []
-    
+
 old_data.extend(arg_list)
 save_to_json_file(old_data, "add_item.json")
