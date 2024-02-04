2307e10ee6b85398e4173f49f2f9f7e388316762
Yassine BAJDA <bajdamr@gmail.com>
2024-02-04 13:35:31 +0000

    class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """Setter for the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0 :
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -35,17 +35,16 @@ class Rectangle:
         """Setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0 :
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
         """Calculates the rectangle area"""
-	return self.__width * self.__height
+        return self.__width * self.__height
 
     def perimeter(self):
-    	"""Calculates the rectangle perimeter"""
-    	if self.__width == 0 or self.__height == 0:
+        """Calculates the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
             return 0
-    	return (self.__width * 2) + (self.__height * 2)
-
+        return (self.__width * 2) + (self.__height * 2)
