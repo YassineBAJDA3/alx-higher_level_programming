5d5081f5d483a6d5cd9ea267352a8df98e1e991a
Yassine BAJDA <bajdamr@gmail.com>
2024-02-06 20:54:40 +0000

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""defining a read_file function"""
+
 
 def read_file(filename=""):
-
+    """
+    Reads a text file (UTF-8 encoded)
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
-
