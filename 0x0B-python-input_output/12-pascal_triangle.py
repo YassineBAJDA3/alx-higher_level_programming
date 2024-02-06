df162a1e9de38e6a3085f170cbdd24ad26245683
Yassine BAJDA <bajdamr@gmail.com>
2024-02-06 23:22:13 +0000

    13. Search and update

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/user/bin/python3
+"""defining function"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    while len(triangle) != n:
+        Triagle = triangle[-1]
+        tmp = [1]
+        for i in range(len(triangle) - 1):
+            tmp.append(Triagle[i] + Triagle[i + 1])
+        tmp.append(1)
+        triangle.append(tmp)
+    return triangle
