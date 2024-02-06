42b9ea189acc05dc5551682831c821988578cecf
Yassine BAJDA <bajdamr@gmail.com>
2024-02-06 23:10:48 +0000

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class representing a student."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def to_json(self, attrs=None):
+        """Initialize a Student instance with
+        first_name, last_name, and age."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance
+        with values from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
