b078c665c0287b45656809d014bb37486a23448d
Yassine BAJDA <bajdamr@gmail.com>
2024-02-06 22:52:18 +0000

    9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,8 +12,4 @@ class Student:
         
     def to_json(self):
         """Retrieve a dictionary representation of a Student instance."""
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        return self.__dict__
